2894617ec083d9014b555051794ddc144e933c67
Nagashima Sayuri <106061832+ngsm-syr@users.noreply.github.com>
2023-06-07 14:07:46 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-// JavaScript Sample
+//javascript
 
 // body の背景色を変える
 function MyFunction01() {
-  var obj = document.body;
+  const obj = document.body;
   obj.style.backgroundColor = 'gray';
 }
 
 // ID要素 の背景色を変える
 function MyFunction02() {
-  var obj = document.getElementById('id01');
+  const obj = document.getElementById('id01');
   obj.style.backgroundColor = 'orange';
 }
 
 // CLASS要素 の背景色を変える
-function MyFunction03() {
-  var lines = document.getElementsByClassName('class01');
-    for( var i=0; i<lines.length; i++ ){
-      lines[i].style.backgroundColor = 'green';
+function   MyFunction03() {
+  const lines = document.getElementsByClassName('class01');
+  for( let i=0; i<lines.length; i++){
+    lines[i].style.backgroundColor = 'green';
   }
 }
 
 // HTML要素 の背景色を変える
 function MyFunction04() {
-  var obj = document.getElementsByTagName("p"); 
-    for( var i=0; i<obj.length; i++ ) {
-      obj[i].style.backgroundColor = "blue"; 
+  const obj = document.getElementsByTagName('p');
+  for( let i=0; i<obj.length; i++ ) {
+    obj[i].style.backgroundColor = 'blue';
   }
 }
